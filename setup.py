a70d68f8cb6f4c5e36de445fadd40aeaef2c52ed
Gerasev Kirill <gerasev.kirill@gmail.com>
2017-07-07 19:55:21 +0300

    v1.0.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-rest-framework-angular-resource-generator',
-    version='0.8.0',
+    version='1.0.0',
     packages=['drf_ng_generator'],
     include_package_data=True,
     license='BSD License',
@@ -29,6 +29,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
