faaa33c9b9e0f5ce749827c6eba72aad04f425bb
Gerasev Kirill <gerasev.kirill@gmail.com>
2017-07-10 16:16:55 +0300

    [-] swagger

--- tests/urls.py
+++ tests/urls.py
@@ -1,10 +1,7 @@
 from rest_framework import routers
 from django.conf.urls import url, include
-from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title='Site API')
 
 urlpatterns = [
-    url('^api/v1/', include('tests.rest.urls')),
-    url(r'^docs/', schema_view)
+    url('^api/v1/', include('tests.rest.urls'))
 ]
