1ef87bb84e5dfeb5515500919a09ff059a6f196b
Gerasev Kirill <gerasev.kirill@gmail.com>
2019-03-05 15:13:12 +0200

    [*] bug with drf insert_into

--- drf_ng_generator/schemas.py
+++ drf_ng_generator/schemas.py
@@ -1,11 +1,29 @@
 from rest_framework import schemas
+import coreapi
+from collections import OrderedDict
 
 from . import helpers
 
 
 
 
+def insert_into(target, keys, value):
+    """
+    Nested dictionary insertion.
 
+    >>> example = {}
+    >>> insert_into(example, ['a', 'b', 'c'], 123)
+    >>> example
+    {'a': {'b': {'c': 123}}}
+    """
+    for key in keys[:-1]:
+        if key not in target:
+            target[key] = {}
+        if not isinstance(target[key], coreapi.Link):
+            target = target[key]
+    #print()
+    #print(target, keys, value)
+    target[keys[-1]] = value
 
 
 class SchemaGenerator(schemas.SchemaGenerator):
@@ -14,6 +32,38 @@ class SchemaGenerator(schemas.SchemaGenerator):
         'update', 'partial_update', 'destroy',
         'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'
     )
+
+    def get_links(self, request=None):
+        """
+        Return a dictionary containing all the links that should be
+        included in the API schema.
+        """
+        links = OrderedDict()
+
+        # Generate (path, method, view) given (path, method, callback).
+        paths = []
+        view_endpoints = []
+        for path, method, callback in self.endpoints:
+            view = self.create_view(callback, method, request)
+            if getattr(view, 'exclude_from_schema', False):
+                continue
+            path = self.coerce_path(path, method, view)
+            paths.append(path)
+            view_endpoints.append((path, method, view))
+
+        # Only generate the path prefix for paths that will be included
+        if not paths:
+            return None
+        prefix = self.determine_path_prefix(paths)
+
+        for path, method, view in view_endpoints:
+            if not self.has_view_permissions(path, method, view):
+                continue
+            link = self.get_link(path, method, view)
+            subpath = path[len(prefix):]
+            keys = self.get_keys(subpath, method, view)
+            insert_into(links, keys, link)
+        return links
 
     def get_link(self, path, method, view):
         link = super(SchemaGenerator, self).get_link(path, method, view)
