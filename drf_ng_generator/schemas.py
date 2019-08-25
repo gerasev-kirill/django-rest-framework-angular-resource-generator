37521d6ef19d745ed910ee081e344f9df4bd166b
Gerasev Kirill <gerasev.kirill@gmail.com>
2019-08-25 15:48:47 +0300

    [*] fix for get_keys in is_custom_action

--- drf_ng_generator/schemas.py
+++ drf_ng_generator/schemas.py
@@ -1,4 +1,5 @@
 from rest_framework import schemas
+from rest_framework.schemas import is_list_view, is_custom_action
 import coreapi
 from collections import OrderedDict
 
@@ -63,10 +64,12 @@ class SchemaGenerator(schemas.SchemaGenerator):
             insert_into(links, keys, link)
         return links
 
+
     def get_link(self, path, method, view):
         link = super(SchemaGenerator, self).get_link(path, method, view)
         link._view = view
         return link
+
 
     def get_key(self, path, method, callback):
         category, action = super(SchemaGenerator, self).get_key(path, method, callback)
@@ -79,3 +82,51 @@ class SchemaGenerator(schemas.SchemaGenerator):
                 category = path[-2]
 
         return (category, action)
+
+
+    def get_keys(self, subpath, method, view):
+        """
+        Return a list of keys that should be used to layout a link within
+        the schema document.
+
+        /users/                   ("users", "list"), ("users", "create")
+        /users/{pk}/              ("users", "read"), ("users", "update"), ("users", "delete")
+        /users/enabled/           ("users", "enabled")  # custom viewset list action
+        /users/{pk}/star/         ("users", "star")     # custom viewset detail action
+        /users/{pk}/groups/       ("users", "groups", "list"), ("users", "groups", "create")
+        /users/{pk}/groups/{pk}/  ("users", "groups", "read"), ("users", "groups", "update"), ("users", "groups", "delete")
+        """
+        if hasattr(view, 'action'):
+            # Viewsets have explicitly named actions.
+            action = view.action
+        else:
+            # Views have no associated action, so we determine one from the method.
+            if is_list_view(subpath, method, view):
+                action = 'list'
+            else:
+                action = self.default_mapping[method.lower()]
+
+        named_path_components = [
+            component for component
+            in subpath.strip('/').split('/')
+            if '{' not in component
+        ]
+
+        if is_custom_action(action):
+            # Custom action, eg "/users/{pk}/activate/", "/users/active/"
+            return named_path_components[:-1] + [action]
+            '''
+            if len(view.action_map) > 1:
+                action = self.default_mapping[method.lower()]
+                if action in self.coerce_method_names:
+                    action = self.coerce_method_names[action]
+                return named_path_components + [action]
+            else:
+                return named_path_components[:-1] + [action]
+            '''
+
+        if action in self.coerce_method_names:
+            action = self.coerce_method_names[action]
+
+        # Default action, eg "/users/", "/users/{pk}/"
+        return named_path_components + [action]
